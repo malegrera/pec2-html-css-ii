08424d4dd0f4034d9f431fc12de2880872ee0dfd
malegrera <malegrera@uoc.edu>
2026-04-28 18:00:40 +0200

    logo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,19 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 import "bootstrap";
+import "@fontsource/itim";
+import "@fontsource/inder";
 /**
  * Write any other JavaScript below
  */
 
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-})();
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav a");
+  const currentPage = location.pathname.split("/").pop();
+
+  links.forEach(link => {
+    if (link.getAttribute("href") === currentPage) {
+      link.classList.add("active");
+    }
+  });
+});
