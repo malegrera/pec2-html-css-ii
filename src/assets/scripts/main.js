16c7fea0bf1dd540a339a6c088da5e1a28a3e02d
malegrera <malegrera@uoc.edu>
2026-05-10 18:27:18 +0200

    js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,44 +9,14 @@ import "bootstrap";
 import "@fontsource/itim";
 import "@fontsource/inder";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 /**
  * Write any other JavaScript below
  */
+import { initNav } from "./nav.js";
+import { initModal } from "./modal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  //nav
-  const links = document.querySelectorAll("nav a");
-  const currentPage = location.pathname.split("/").pop();
-
-  links.forEach(link => {
-    if (link.getAttribute("href") === currentPage) {
-      link.classList.add("active");
-    }
-  });
-
-  //modal
-  const modal = document.getElementById("artesanoModal");
-
-  if (modal) {
-
-    modal.addEventListener("show.bs.modal", (event) => {
-
-      const button = event.relatedTarget;
-      const name = button.dataset.name;
-      const role = button.dataset.role;
-      const description = button.dataset.description;
-      const taller = button.dataset.taller;
-      document.getElementById("modalTitle").textContent = name;
-      document.getElementById("modalRole").textContent = role;
-      document.getElementById("modalDescription").textContent = description;
-      document.getElementById("modalTaller").textContent = taller;
-
-    });
-
-  }
-
-
-
-
+  initNav();
+  initModal();
 });
